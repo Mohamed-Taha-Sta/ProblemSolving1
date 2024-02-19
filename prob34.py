60197e302945d912854e0e3de979544aac1b4c8a
Taha <mohamedtahasta@gmail.com>
2024-02-19 16:56:02 +0100

    Pushing today's problems away! :D

---
+++ prob34.py
@@ -0,0 +1,21 @@
+
+# 231
+
+def isPowerOfTwo(n: int) -> bool:
+    if n == 0:
+        return False
+    elif n == 1:
+        return True
+    else:
+        i: int = 1
+        while i <= n:
+            if i == n:
+                return True
+            else:
+                i *= 2
+    return False
+
+if __name__ == '__main__':
+    print(isPowerOfTwo(n = 1))
+    # print(isPowerOfTwo(n = 16))
+    # print(isPowerOfTwo(n = 3))
